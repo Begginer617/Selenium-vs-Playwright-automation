aaf610426ebe7097fc7d8ee9dc5c5088253aff9a
Milosz Skorupski <skorupskimilosz@gmail.com>
2026-03-20 11:39:58 +0100

    adding fixtures for selenium and playwiorght using pytest

---
+++ pages/playwright/home_page.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+from pages.selenium.base_page import BasePage
+
+
+
+
+"""
+Base Page Object for each page
+"""
+
+
+class HomePage(BasePage):
+    pass
