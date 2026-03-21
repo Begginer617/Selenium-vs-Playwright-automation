916e1e5bb9a8be408a867129ca6b303b3f67ad51
Milosz Skorupski <skorupskimilosz@gmail.com>
2026-03-21 21:57:54 +0100

    building xpath for login and errors

--- pages/selenium/login_page_selenium.py
+++ pages/selenium/login_page_selenium.py
@@ -2,27 +2,32 @@ from selenium.webdriver.common import by
 from selenium.webdriver.common.by import By
 from pages.selenium.base_page_selenium import BasePage
 
+
 class LoginPage(BasePage):
     """
-    lOCALISATORS Errors
+    Web element locators -  Errors
     """
+
+    # --- BŁĘDY W PODSUMOWANIU
+    # (Validation Summary)
+    NAME_ERROR_VALIDATION_SUMMARY_LIST = (By.XPATH,
+                                          "//div[contains(@class, 'k-validation-summary')]//li[contains(text(), 'First and Last name')]")
+    EMAIL_ERROR_TVALIDATION_SUMMARY_LIST = (By.XPATH,
+                                            "//div[contains(@class, 'k-validation-summary')]//li[contains(text(), 'Email is required')]")
+    PASS_ERROR_VALIDATION_SUMMARY_LIST = (By.XPATH,
+                                          "//div[contains(@class, 'k-validation-summary')]//li[contains(text(), 'Please enter password')]")
+    PASSWORD_REQUIREMENTS_VALIDATION_SUMMARY_LIST = (By.XPATH, "")
+
     # --- BŁĘDY POD INPUTAMI (Dół) ---
     NAME_ERROR_BOTTOM = (By.ID, "FirstAndLastName-error")
     EMAIL_ERROR_BOTTOM = (By.ID, "Email-error")
     PASS_ERROR_BOTTOM = (By.ID, "Password-error")
-
-    # --- BŁĘDY W PODSUMOWANIU (Góra - Twoja strzałka) ---
-    # Szukamy konkretnego tekstu wewnątrz czerwonej ramki (Validation Summary)
-    NAME_ERROR_TOP = (By.XPATH,
-                      "//div[contains(@class, 'k-validation-summary')]//li[contains(text(), 'First and Last name')]")
-    EMAIL_ERROR_TOP = (By.XPATH,
-                       "//div[contains(@class, 'k-validation-summary')]//li[contains(text(), 'Email is required')]")
-    PASS_ERROR_TOP = (By.XPATH,
-                      "//div[contains(@class, 'k-validation-summary')]//li[contains(text(), 'Please enter password')]")
-
+    PLEASE_ENTER_FIRST_AND_LAST_NAME_SEPARATED_BY_A_SPACE_BOTTOM = (By.XPATH, "")
+    EMAIL_IS_NOT_VALID_EMAIL_BOTTOM = (By.XPATH, "")
+    PASSWORD_REQUIREMENTS_BOTTOM = (By.XPATH, "")
 
     """
-    lOCALISATORS FORM Register and Login
+     Web element locators - FORM Register and Login
     """
     LOGIN_BUTTON = (By.XPATH, "//a[@href='/kendo-ui/eshop/Account/Login']")
     REGISTER_SUBMIT_BUTTON = (By.XPATH, "//button[@type='submit']")
@@ -36,14 +41,14 @@ class LoginPage(BasePage):
 
     def trigger_required_errors(self):
         """Wywołuje błędy 'Email is required' itp. poprzez kliknięcie w pusty formularz."""
-        self.wait_for_visible(self.NAME_INPUT).clear()
+        self.wait_for_visible(self.FIRST_AND_LAST_NAME_INPUT).clear()
         self.wait_for_visible(self.EMAIL_INPUT).clear()
         self.wait_for_visible(self.PASSWORD_INPUT).clear()
         self.click_register()
 
     def trigger_format_errors(self, incorrect_name, incorrect_email, short_password):
         """Metoda wpisuje błędne dane, aby wymusić komunikaty walidacji."""
-        name_field = self.wait_for_visible(self.NAME_INPUT)
+        name_field = self.wait_for_visible(self.FIRST_AND_LAST_NAME_INPUT)
         name_field.clear()
         name_field.send_keys(incorrect_name)
 
@@ -56,8 +61,3 @@ class LoginPage(BasePage):
         pass_field.send_keys(short_password)
 
         self.click_register()
-
-
-
-
-
