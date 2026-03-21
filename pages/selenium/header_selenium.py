d3dad08e784e712eea9e6acc09a73f71506680d7
Milosz Skorupski <skorupskimilosz@gmail.com>
2026-03-21 14:06:48 +0100

    chanagin names of folder structure

--- pages/selenium/header_selenium.py
+++ pages/selenium/header_selenium.py
@@ -1,12 +1,17 @@
-from selenium.webdriver.common.by import By
-from pages.selenium.base_page import BasePage
+from driver import driver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+
+from pages.selenium.base_page_selenium import BasePage
 
 
-class Header(BasePage):
+
+
+
+class header_selenium(BasePage):
     """
-     lOCALISATORS HEADER
-     """
+    lOCALISATORS HEADER
+    """
 
     SHOPPING_CART_BUTTON = driver.find_element(By.XPATH, "//a[contains(@href, 'ShoppingCart')]")
     SHOPPING_CART_COUNT = driver.find_element(By.XPATH, "//span[@id='shopping-cart-badge']")
@@ -14,9 +19,11 @@ class Header(BasePage):
                                         "//span[contains(@class, 'k-i-user')]/ancestor::span[contains(@class, 'k-menu-link')]")
     PROFILE_ICONE_DROP_DOWN = driver.find_element(By.XPATH, "//a[contains(@href, '/kendo-ui/eshop/Account/Profile')]")
 
+    def go_to_profile_icon(self):
 
-def go_to_profile_icon(self):
-    user_element = self.wait_for_visible(self.PROFILE_ICONE)
+        user_element = self.wait_for_visible(self.PROFILE_ICONE)
     ActionChains(self.driver).move_to_element(user_element).perform()
 
+    def click_cart(self):
 
+        self.click(self.SHOPPING_CART_BTN)
