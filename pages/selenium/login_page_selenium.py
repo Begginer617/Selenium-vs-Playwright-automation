d8c746b0855c5669cfd4999020e6e3bf14539202
Milosz Skorupski <skorupskimilosz@gmail.com>
2026-03-21 23:54:36 +0100

    building 1 test with assertions for login page

--- pages/selenium/login_page_selenium.py
+++ pages/selenium/login_page_selenium.py
@@ -1,4 +1,3 @@
-from selenium.webdriver.common import by
 from selenium.webdriver.common.by import By
 from pages.selenium.base_page_selenium import BasePage
 
@@ -10,55 +9,67 @@ class LoginPage(BasePage):
 
     # --- BŁĘDY W PODSUMOWANIU
     # (Validation Summary)
-    NAME_ERROR_VALIDATION_SUMMARY_LIST = (By.XPATH,
-                                          "//div[contains(@class, 'k-validation-summary')]//li[contains(text(), 'First and Last name')]")
-    EMAIL_ERROR_VALIDATION_SUMMARY_LIST = (By.XPATH,
-                                           "//div[contains(@class, 'k-validation-summary')]//li[contains(text(), 'Email is required')]")
-    PASS_ERROR_VALIDATION_SUMMARY_LIST = (By.XPATH,
-                                          "//div[contains(@class, 'k-validation-summary')]//li[contains(text(), 'Please enter password')]")
-    PASSWORD_REQUIREMENTS_VALIDATION_SUMMARY_LIST = (By.XPATH,
-                                                     "//a[contains(text(), 'Your password must be at least 8 symbols ')]")
 
+    # Stan: Empty fields
+    EMPTY_FIRST_AND_LAST_NAME_VALIDATION_SUMMARY_LIST = (By.XPATH, "//li[contains(., 'First and Last name')]")
+    EMPTY_EMAIL_ERROR_VALIDATION_SUMMARY_LIST = (By.XPATH, "//li[contains(., 'Email is required')]")
+
+    EMPTY_PASSWORD_ERROR_VALIDATION_SUMMARY_LIST = (By.XPATH, "//li[contains(., 'Please enter password')]")
+
+    # Stan: Wrong Formats
+    FIRST_AND_LAST_NAME_SEPARATED_BY_A_SPACE_VALIDATION_SUMMARY_LIST = (By.XPATH,
+                                                                        "//a[@data-field='FirstAndLastName' and contains(text(), 'separated by a space')]")
+    EMAIL_IS_NOT_VALID_VALIDATION_SUMMARY_LIST = (By.XPATH,
+                                                  "//a[@data-field='Email' and text()='Email is not valid email']")
+    PASSWORD_REQUIREMENTS_VALIDATION_SUMMARY_LIST = (By.XPATH,
+                                                     "//a[@data-field='Password' and contains(text(), '8 symbols')]")
+
+    """
+    Web element locators - Errors under inputs (Dół)
+    """
     # --- BŁĘDY POD INPUTAMI (Dół) ---
-    NAME_ERROR_BOTTOM = (By.ID, "FirstAndLastName-error")
-    EMAIL_ERROR_BOTTOM = (By.ID, "Email-error")
-    PASS_ERROR_BOTTOM = (By.XPATH, "//span[@id='Password-error' and contains(text(), 'Please enter password)]")
-    PLEASE_ENTER_FIRST_AND_LAST_NAME_SEPARATED_BY_A_SPACE_BOTTOM = (By.XPATH, "//span[@id='FirstAndLastName-error' and contains(text(), 'separated by a space')]")
+    # Stan: Empty fields
+    EMPTY_FIRST_AND_LAST_NAME_BOTTOM = (By.ID, "FirstAndLastName-error")
+    EMPTY_EMAIL_ERROR_BOTTOM = (By.ID, "Email-error")
+    EMPTY_PASSWORD_ERROR_BOTTOM = (By.XPATH,
+                                   "//span[@id='Password-error' and contains(text(), 'Please enter password')]")
+
+    # Stan: Wrong Formats
+    FIRST_AND_LAST_NAME_SEPARATED_BY_A_SPACE_BOTTOM = (By.XPATH,
+                                                       "//span[@id='FirstAndLastName-error' and contains(text(), 'separated by a space')]")
     EMAIL_IS_NOT_VALID_EMAIL_BOTTOM = (By.XPATH, "//span[@id='Email-error' and text()='Email is not valid email']")
     PASSWORD_REQUIREMENTS_BOTTOM = (By.XPATH, "//span[@id='Password-error' and contains(text(), '8 symbols')]")
 
     """
      Web element locators - FORM Register and Login
     """
+
+    FIRST_AND_LAST_NAME_INPUT = (By.ID, "FirstAndLastName")
+    EMAIL_INPUT = (By.ID, "Email")
+    PASSWORD_INPUT = (By.ID, "Password")
+
+    """
+    Web element locators - Buttons login page
+    """
+
     LOGIN_BUTTON = (By.XPATH, "//a[@href='/kendo-ui/eshop/Account/Login']")
     REGISTER_SUBMIT_BUTTON = (By.XPATH, "//button[@type='submit']")
-    FIRST_AND_LAST_NAME_INPUT = (By.XPATH, "//input[@id='FirstAndLastName']")
-    EMAIL_INPUT = (By.XPATH, "//input[@id='Email']")
-    PASSWORD_INPUT = (By.XPATH, "//input[@id='Password']")
 
     """
     Methodes
     """
 
+    def click_register(self):
+        self.wait_for_clickable(self.REGISTER_SUBMIT_BUTTON).click()
+
     def trigger_required_errors(self):
-        """Wywołuje błędy 'Email is required' itp. poprzez kliknięcie w pusty formularz."""
         self.wait_for_visible(self.FIRST_AND_LAST_NAME_INPUT).clear()
         self.wait_for_visible(self.EMAIL_INPUT).clear()
         self.wait_for_visible(self.PASSWORD_INPUT).clear()
         self.click_register()
 
     def trigger_format_errors(self, incorrect_name, incorrect_email, short_password):
-        """Metoda wpisuje błędne dane, aby wymusić komunikaty walidacji."""
-        name_field = self.wait_for_visible(self.FIRST_AND_LAST_NAME_INPUT)
-        name_field.clear()
-        name_field.send_keys(incorrect_name)
-
-        email_field = self.wait_for_visible(self.EMAIL_INPUT)
-        email_field.clear()
-        email_field.send_keys(incorrect_email)
-
-        pass_field = self.wait_for_visible(self.PASSWORD_INPUT)
-        pass_field.clear()
-        pass_field.send_keys(short_password)
-
+        self.wait_for_visible(self.FIRST_AND_LAST_NAME_INPUT).send_keys(incorrect_name)
+        self.wait_for_visible(self.EMAIL_INPUT).send_keys(incorrect_email)
+        self.wait_for_visible(self.PASSWORD_INPUT).send_keys(short_password)
         self.click_register()
