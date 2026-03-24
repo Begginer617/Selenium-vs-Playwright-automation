a2b24e70cf26297bfd5a1073a61c6546c81fc083
Milosz Skorupski <skorupskimilosz@gmail.com>
2026-03-24 12:49:34 +0100

    fixing names of files, class and methodes due the incorrect naming

---
+++ selenium_tests/test_registration_page_selenium.py
@@ -0,0 +1,72 @@
+import pytest
+from pages.selenium.registration_page_selenium import LoginPage
+
+
+@pytest.fixture
+def registration_page(driver):
+    return LoginPage(driver)
+
+
+def test_registration_form_validation(driver, registration_page):
+    # 1. Wejdź na stronę
+    driver.get("https://demos.telerik.com/kendo-ui/eshop/Account/Register")
+
+    # --- ETAP 1: TEST PUSTEGO FORMULARZA ---
+    # Klikamy register bez wpisywania czegokolwiek
+    registration_page.trigger_required_errors()
+
+    # Asercje dla Góry (Summary)
+    assert registration_page.wait_for_visible(registration_page.EMPTY_FIRST_AND_LAST_NAME_VALIDATION_SUMMARY_LIST).is_displayed()
+    assert registration_page.wait_for_visible(registration_page.EMPTY_EMAIL_ERROR_VALIDATION_SUMMARY_LIST).is_displayed()
+    assert registration_page.wait_for_visible(registration_page.EMPTY_PASSWORD_ERROR_VALIDATION_SUMMARY_LIST).is_displayed()
+
+    # Asercje dla Dołu (Pod inputami)
+    assert registration_page.wait_for_visible(registration_page.EMPTY_FIRST_AND_LAST_NAME_BOTTOM).is_displayed()
+    assert "Email is required" in registration_page.wait_for_visible(registration_page.EMPTY_EMAIL_ERROR_BOTTOM).text
+    assert "Please enter password" in registration_page.wait_for_visible(registration_page.EMPTY_PASSWORD_ERROR_BOTTOM).text
+
+    # --- ETAP 2: TEST BŁĘDNEGO FORMATU ---
+    # Odświeżamy stronę, żeby wyczyścić stan
+    driver.refresh()
+
+    registration_page.trigger_format_errors(
+        incorrect_name="Tester",
+        incorrect_email="zly_email.pl",
+        short_password="123"
+    )
+
+    # Asercje dla Góry
+    assert registration_page.wait_for_visible(
+        registration_page.FIRST_AND_LAST_NAME_SEPARATED_BY_A_SPACE_VALIDATION_SUMMARY_LIST).is_displayed()
+    assert registration_page.wait_for_visible(registration_page.EMAIL_IS_NOT_VALID_VALIDATION_SUMMARY_LIST).is_displayed()
+    assert registration_page.wait_for_visible(registration_page.PASSWORD_REQUIREMENTS_VALIDATION_SUMMARY_LIST).is_displayed()
+
+    # Asercje dla Dołu
+    assert "separated by a space" in registration_page.wait_for_visible(
+        registration_page.FIRST_AND_LAST_NAME_SEPARATED_BY_A_SPACE_BOTTOM).text
+    assert "not valid email" in registration_page.wait_for_visible(registration_page.EMAIL_IS_NOT_VALID_EMAIL_BOTTOM).text
+    assert "8 symbols" in registration_page.wait_for_visible(registration_page.PASSWORD_REQUIREMENTS_BOTTOM).text
+
+    print("✅ Sukces: Wszystkie 12 punktów walidacji sprawdzonych!")
+
+    @pytest.fixture
+    def test_login_page_positive(driver, login_page):
+        # 1. Wejdź na stronę register
+        driver.get("https://demos.telerik.com/kendo-ui/eshop/Account/Register")
+        # 2. Kliknij w button login na stronie rejestracji i zrob asercje
+        driver.get("https://demos.telerik.com/kendo-ui/eshop/Account/Login")
+        # 2. Kliknij pole Email i wyczyścić pole email
+        # 3. wpisz prawidłowy email
+        # 3. Kliknij password i wyczyścić pole password
+        #4.  Kliknij pole password i wpisz passsword
+
+
+
+
+
+#
+
+
+
+
+
