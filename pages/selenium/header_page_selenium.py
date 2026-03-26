9bd3998077cdc838a0ede3b80b418ed8c9ea9518
Milosz Skorupski <skorupskimilosz@gmail.com>
2026-03-26 17:46:33 +0100

    fixing issues with hover, added methoded with js

--- pages/selenium/header_page_selenium.py
+++ pages/selenium/header_page_selenium.py
@@ -3,24 +3,43 @@ from selenium.webdriver.common.by import By
 from pages.selenium.base_page_selenium import BasePage
 
 class HeaderSeleniumPage(BasePage):
-    # 1. Definiujemy SAME ADRESY (Lokatory). To są krotki (tuples).
+    # 1. Lokatory
     SHOPPING_CART_BTN = (By.XPATH, "//a[contains(@href, 'ShoppingCart')]")
     SHOPPING_CART_COUNT = (By.ID, "shopping-cart-badge")
     PROFILE_ICON = (By.XPATH, "//span[contains(@class, 'k-i-user')]/ancestor::span[contains(@class, 'k-menu-link')]")
     PROFILE_DROPDOWN_LINK = (By.XPATH, "//a[contains(@href, '/kendo-ui/eshop/Account/Profile')]")
+    CATEGORIES_MENU = (By.XPATH, "//span[normalize-space()='Categories']")
+    ACCESSORIES_LINK = (By.XPATH, "//a[contains(@class, 'k-menu-link') and normalize-space()='Accessories']")
 
-
-    # 2. Metody wykonujące akcje
+    # 2. Metody
     def go_to_profile_icon(self):
-        # Czekamy na ikonę i robimy Hover
         user_element = self.wait_for_visible(self.PROFILE_ICON)
         ActionChains(self.driver).move_to_element(user_element).perform()
 
     def click_profile_dropdown(self):
-        # Klikamy w link profilu (BasePage zajmie się czekaniem na klikalność)
         self.click(self.PROFILE_DROPDOWN_LINK)
 
     def click_cart(self):
         self.click(self.SHOPPING_CART_BTN)
 
+    def open_accessories_category(self):
+        # 1. Czekamy na Categories i najeżdżamy
+        categories = self.wait_for_visible(self.CATEGORIES_MENU)
 
+        # Przewijamy do elementu na wszelki wypadek
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", categories)
+
+        # Wykonujemy hover z kliknięciem i przytrzymaniem (czasem pomaga w Kendo)
+        actions = ActionChains(self.driver)
+        actions.move_to_element(categories).click_and_hold(categories).pause(1).release().perform()
+
+        # 2. Sprawdzamy widoczność Accessories
+        # Jeśli tu wciąż wywala Timeout, spróbuj zamienić wait_for_visible na wait_for_clickable
+        try:
+            element = self.wait_for_visible(self.ACCESSORIES_LINK)
+            element.click()
+        except:
+            # OSTATNIA DESKA RATUNKU: Kliknięcie przez JavaScript (ominie problem widoczności)
+            print("Standardowe kliknięcie zawiodło, próbuję JS...")
+            accessories = self.driver.find_element(*self.ACCESSORIES_LINK)
+            self.driver.execute_script("arguments[0].click();", accessories)
--- selenium_tests/test_header_page_selenium.py
+++ selenium_tests/test_header_page_selenium.py
@@ -1,5 +1,29 @@
 def test_header_page_selenium(home_page_selenium, login_page, header_page_selenium):
+    # 1. Otwórz stronę logowania
     home_page_selenium.open_login_page()
-    # login_page.login_as_admin()
+
+    # 2. Zaloguj się (używamy fixtury login_page)
+    login_page.login_as_admin()
+
+    # 3. Kliknij w koszyk używając Twojej metody z klasy HeaderSeleniumPage
+    # Używamy nazwy fixtury: header_page_selenium
+    header_page_selenium.click_cart()
+
+    # Opcjonalnie: sprawdź czy jesteś w koszyku
+    assert "ShoppingCart" in header_page_selenium.driver.current_url
+
+    # 4. POWRÓT NA STRONĘ GŁÓWNĄ (To jest kluczowe!)
+    # Zamiast home_page_selenium.open_login_page(), używamy bezpośredniego przejścia:
+    header_page_selenium.driver.get("https://demos.telerik.com/kendo-ui/eshop")
+
+    # Opcjonalnie: upewnij się, że strona się załadowała
+    assert "https://demos.telerik.com/kendo-ui/eshop" in header_page_selenium.driver.current_url
+
+    # 5. TERAZ HOVER I KLIKNIĘCIE
+    # Skoro jesteś na właściwej stronie, menu "Categories" powinno być widoczne i działać
+    header_page_selenium.open_accessories_category()
+
+    # 6. Finalna asercja
+    assert "Home/Accessories" in header_page_selenium.driver.current_url
 
 
