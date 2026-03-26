ecdcb07299b36eca7a2f07a96ecd8f9a1b46a39f
Milosz Skorupski <skorupskimilosz@gmail.com>
2026-03-26 18:17:21 +0100

    fixing issues with hover, added methoded with js, implemented methode with js and click

--- pages/selenium/header_page_selenium.py
+++ pages/selenium/header_page_selenium.py
@@ -2,44 +2,51 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from pages.selenium.base_page_selenium import BasePage
 
+
 class HeaderSeleniumPage(BasePage):
-    # 1. Lokatory
+    # --- LOKATORY ---
     SHOPPING_CART_BTN = (By.XPATH, "//a[contains(@href, 'ShoppingCart')]")
-    SHOPPING_CART_COUNT = (By.ID, "shopping-cart-badge")
-    PROFILE_ICON = (By.XPATH, "//span[contains(@class, 'k-i-user')]/ancestor::span[contains(@class, 'k-menu-link')]")
-    PROFILE_DROPDOWN_LINK = (By.XPATH, "//a[contains(@href, '/kendo-ui/eshop/Account/Profile')]")
     CATEGORIES_MENU = (By.XPATH, "//span[normalize-space()='Categories']")
+
+    # Lokatory kategorii
     ACCESSORIES_LINK = (By.XPATH, "//a[contains(@class, 'k-menu-link') and normalize-space()='Accessories']")
+    BIKES_LINK = (By.XPATH, "//a[contains(@class, 'k-menu-link') and normalize-space()='Bikes']")
+    CLOTHES_LINK = (By.XPATH, "//a[contains(@class, 'k-menu-link') and normalize-space()='Clothes']")
+    COMPONENTS_LINK = (By.XPATH, "//a[contains(@class, 'k-menu-link') and normalize-space()='Components']")
 
-    # 2. Metody
-    def go_to_profile_icon(self):
-        user_element = self.wait_for_visible(self.PROFILE_ICON)
-        ActionChains(self.driver).move_to_element(user_element).perform()
-
-    def click_profile_dropdown(self):
-        self.click(self.PROFILE_DROPDOWN_LINK)
+    # --- METODY PODSTAWOWE ---
 
     def click_cart(self):
         self.click(self.SHOPPING_CART_BTN)
 
-    def open_accessories_category(self):
-        # 1. Czekamy na Categories i najeżdżamy
-        categories = self.wait_for_visible(self.CATEGORIES_MENU)
+    # --- METODY KATEGORII ---
 
-        # Przewijamy do elementu na wszelki wypadek
+    def open_accessories_category(self):
+        self._hover_and_click(self.ACCESSORIES_LINK)
+
+    def open_bikes_category(self):
+        self._hover_and_click(self.BIKES_LINK)
+
+    def open_clothes_category(self):
+        self._hover_and_click(self.CLOTHES_LINK)
+
+    def open_components_category(self):
+        self._hover_and_click(self.COMPONENTS_LINK)
+
+    # Wspólna logika dla Hovera
+    def _hover_and_click(self, locator):
+        categories = self.wait_for_visible(self.CATEGORIES_MENU)
+        # Przewijamy do menu, żeby było widoczne
         self.driver.execute_script("arguments[0].scrollIntoView(true);", categories)
 
-        # Wykonujemy hover z kliknięciem i przytrzymaniem (czasem pomaga w Kendo)
         actions = ActionChains(self.driver)
-        actions.move_to_element(categories).click_and_hold(categories).pause(1).release().perform()
+        actions.move_to_element(categories).pause(0.5).perform()
 
-        # 2. Sprawdzamy widoczność Accessories
-        # Jeśli tu wciąż wywala Timeout, spróbuj zamienić wait_for_visible na wait_for_clickable
         try:
-            element = self.wait_for_visible(self.ACCESSORIES_LINK)
-            element.click()
+            # Próbujemy normalnie kliknąć
+            self.click(locator)
         except:
-            # OSTATNIA DESKA RATUNKU: Kliknięcie przez JavaScript (ominie problem widoczności)
-            print("Standardowe kliknięcie zawiodło, próbuję JS...")
-            accessories = self.driver.find_element(*self.ACCESSORIES_LINK)
-            self.driver.execute_script("arguments[0].click();", accessories)
+            # Jeśli menu zniknie lub Selenium 'zgubi' element - klikamy przez JS
+            print(f"Standardowe kliknięcie zawiodło, używam JS dla: {locator}")
+            element = self.driver.find_element(*locator)
+            self.driver.execute_script("arguments[0].click();", element)
