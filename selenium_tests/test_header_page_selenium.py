063b8b00f95e5de90fbc21d47ae6b8434f14def2
Milosz Skorupski <skorupskimilosz@gmail.com>
2026-03-26 18:47:37 +0100

    fixing issues

--- selenium_tests/test_header_page_selenium.py
+++ selenium_tests/test_header_page_selenium.py
@@ -6,21 +6,29 @@ def test_header_page_selenium(home_page_selenium, login_page, header_page_seleni
     # 2. Test Koszyka
     header_page_selenium.click_cart()
     assert "ShoppingCart" in header_page_selenium.get_url()
+
     # 3. Powrót na główną przed testem kategorii
     header_page_selenium.open_home_page()
-    assert "ShoppingCart" in header_page_selenium.get_url()
+    assert "eshop" in header_page_selenium.get_url()
+
     # 4. Test Accessories
     header_page_selenium.open_accessories_category()
-    assert "ShoppingCart" in header_page_selenium.get_url()
+    assert "Home/Accessories" in header_page_selenium.get_url()
+
     # 5. Test Bikes
     header_page_selenium.open_home_page()
     header_page_selenium.open_bikes_category()
-    assert "ShoppingCart" in header_page_selenium.get_url()
-    # 6. Test Clothes (URL to /Home/Clothing)
+    # POPRAWIONE: Sprawdzamy czy w URL są rowery (Bikes)
+    assert "Home/Bikes" in header_page_selenium.get_url()
+
+    # 6. Test Clothes
     header_page_selenium.open_home_page()
     header_page_selenium.open_clothes_category()
-    assert "ShoppingCart" in header_page_selenium.get_url()
+    # POPRAWIONE: Sprawdzamy czy w URL są ubrania (Clothing)
+    assert "Home/Clothing" in header_page_selenium.get_url()
+
     # 7. Test Components
     header_page_selenium.open_home_page()
     header_page_selenium.open_components_category()
-    assert "ShoppingCart" in header_page_selenium.get_url()
+    # POPRAWIONE: Sprawdzamy czy w URL są komponenty (Components)
+    assert "Home/Components" in header_page_selenium.get_url()
