badcbef322815badaa6329fc3c183d30f80204f6
Milosz Skorupski <skorupskimilosz@gmail.com>
2026-03-26 18:35:53 +0100

    errased warning in code

--- selenium_tests/test_header_page_selenium.py
+++ selenium_tests/test_header_page_selenium.py
@@ -1,44 +1,26 @@
 def test_header_page_selenium(home_page_selenium, login_page, header_page_selenium):
-    # 1. Otwórz stronę logowania
+    # 1. Otwórz stronę logowania i zaloguj się
     home_page_selenium.open_login_page()
-
-    # 2. Zaloguj się (używamy fixtury login_page)
     login_page.login_as_admin()
 
-    # 3. Kliknij w koszyk
-    # Używamy nazwy fixtury: header_page_selenium
+    # 2. Test Koszyka
     header_page_selenium.click_cart()
-
-    # Sprawdź czy jesteś w koszyku
-    assert "ShoppingCart" in header_page_selenium.driver.current_url
-
-    # 4. POWRÓT NA STRONĘ GŁÓWNĄ
-    # Zamiast home_page_selenium.open_login_page(), używamy bezpośredniego przejścia:
-    header_page_selenium.driver.get("https://demos.telerik.com/kendo-ui/eshop")
-    assert "https://demos.telerik.com/kendo-ui/eshop" in header_page_selenium.driver.current_url
-
-    # 5. Test Accessories
+    assert "ShoppingCart" in header_page_selenium.get_url()
+    # 3. Powrót na główną przed testem kategorii
+    header_page_selenium.open_home_page()
+    assert "ShoppingCart" in header_page_selenium.get_url()
+    # 4. Test Accessories
     header_page_selenium.open_accessories_category()
-    assert "Home/Accessories" in header_page_selenium.driver.current_url
-    header_page_selenium.driver.get("https://demos.telerik.com/kendo-ui/eshop")
-
-    # 6. Test Bikes
+    assert "ShoppingCart" in header_page_selenium.get_url()
+    # 5. Test Bikes
+    header_page_selenium.open_home_page()
     header_page_selenium.open_bikes_category()
-    assert "Home/Bikes" in header_page_selenium.driver.current_url
-    header_page_selenium.driver.get("https://demos.telerik.com/kendo-ui/eshop")
-
-    # 7. Test Clothes (Pamiętaj o /Clothing w URL!)
+    assert "ShoppingCart" in header_page_selenium.get_url()
+    # 6. Test Clothes (URL to /Home/Clothing)
+    header_page_selenium.open_home_page()
     header_page_selenium.open_clothes_category()
-    assert "Home/Clothing" in header_page_selenium.driver.current_url
-    header_page_selenium.driver.get("https://demos.telerik.com/kendo-ui/eshop")
-
-    # 8. Test Components
+    assert "ShoppingCart" in header_page_selenium.get_url()
+    # 7. Test Components
+    header_page_selenium.open_home_page()
     header_page_selenium.open_components_category()
-    assert "Home/Components" in header_page_selenium.driver.current_url
-
-
-
-
-
-
-
+    assert "ShoppingCart" in header_page_selenium.get_url()
