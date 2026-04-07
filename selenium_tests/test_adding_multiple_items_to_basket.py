f50949177dedd0c6a6a5dad42fecaf22c75960b0
Begginer617 <skorupskimilosz@gmail.com>
2026-04-07 13:56:40 +0200

    fix test_multiple_products_total_calculation

---
+++ selenium_tests/test_adding_multiple_items_to_basket.py
@@ -0,0 +1,13 @@
+def test_multiple_products_total_calculation(home_page_selenium, login_page_selenium, product_page_selenium):
+    # 1. Przygotowanie
+    home_page_selenium.open_login_page()
+    login_page_selenium.login_as_admin()
+
+    # 2. Czyszczenie (teraz już działa!)
+    product_page_selenium.clear_cart()
+
+    # 3. Wejście w kategorię
+    product_page_selenium.open_mountain_bikes()
+
+    # 4. Wielkie liczenie
+    product_page_selenium.add_multiple_products_and_verify_total(count=5)
