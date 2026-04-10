e18a684e46115bd6e5ee136b3615908759e020ee
Begginer617 <skorupskimilosz@gmail.com>
2026-04-10 18:37:19 +0200

    stable version of playwirght (homepage,basepage) added decarators for allure reports and final comparison

---
+++ pages/playwright/registration_page_playwright.py
@@ -0,0 +1,2 @@
+class RegistrationPagePw:
+    pass
--- selenium_tests/test_home_page_selenium.py
+++ selenium_tests/test_home_page_selenium.py
@@ -1,10 +1,17 @@
+import allure
+
+
+@allure.parent_suite("Selenium Framework")
+@allure.suite("E-shop Tests")
 def test_homepage_title(home_page_selenium, login_page_selenium):
     # 1. Otwieramy i logujemy się
     home_page_selenium.open_login_page()
     login_page_selenium.login_as_admin()
 
-    # 2. Czekamy na URL (teraz bez slasha w zmiennej przejdzie od razu!)
+    # 2. Czekamy na załadowanie strony głównej po zalogowaniu
     home_page_selenium.wait_for_url(home_page_selenium.HOME_PAGE_URL)
 
-    # 3. Sprawdzamy tytuł lub URL
-    assert home_page_selenium.HOME_PAGE_URL in login_page_selenium.driver.current_url
+    # 3. Sprawdzamy czy tytuł jest poprawny (zgodnie z nazwą testu)
+    actual_title = home_page_selenium.get_title()
+    expected_title = "Home Page - Web"
+    assert expected_title in actual_title, f"Błąd! Oczekiwano '{expected_title}', a otrzymano '{actual_title}'"
