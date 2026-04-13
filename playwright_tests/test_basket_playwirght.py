ab944e4fec6bc933ad1b5380f95bec4da41540d2
Begginer617 <skorupskimilosz@gmail.com>
2026-04-13 16:53:17 +0200

    test test_search_product_exists_pw complted, all test working

--- playwright_tests/test_basket_playwirght.py
+++ playwright_tests/test_basket_playwirght.py
@@ -3,14 +3,14 @@ import allure
 
 @allure.parent_suite("Playwright Framework")
 @allure.suite("E-shop Tests")
-def test_add_to_cart_flow_pw(home_page_pw, login_page_pw, products_page_pw):
+def test_add_to_cart_flow_pw(home_page_pw, login_page_pw, product_page_pw):
     home_page_pw.open_login_page_pw()
     login_page_pw.login_as_admin_pw()
 
     # WYCZYŚĆ KOSZYK
-    products_page_pw.clear_cart_pw()
+    product_page_pw.clear_cart_pw()
 
     # DODAJ JEDEN I WERYFIKUJ
-    products_page_pw.open_main_pw()\
+    product_page_pw.open_bikes_main_link_pw()\
                     .open_mountain_bikes_pw()\
                     .add_first_product_to_cart_and_verify_pw()
